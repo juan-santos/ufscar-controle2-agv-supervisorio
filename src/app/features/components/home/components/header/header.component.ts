b5c527159b6234843849aed9397af847df4473c5
Juan Santos <juan.h.santos@gmail.com>
2023-03-09 22:02:39 -0300

    feat: adicionando icones ao projeto

--- src/app/features/components/home/components/header/header.component.ts
+++ src/app/features/components/home/components/header/header.component.ts
@@ -1,28 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { map, Observable, take } from 'rxjs';
 import { ResizebledService } from './../../../../../features/shared/resizebled/service/resizebled/resizebled.service';
-import { MenuService } from './../../../../services/menu/menu.service';
+import { MenuInterface, MenuService } from './../../../../services/menu/menu.service';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss'],
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-    public isMobile = false;
+  public isMobile = false;
 
-    constructor(
-        private readonly menuService: MenuService,
-        private readonly resizebledService: ResizebledService
-    ) {}
+  constructor(
+    private readonly menuService: MenuService,
+    private readonly resizebledService: ResizebledService
+  ) { }
 
-    public hideSidebar(): void {
-        this.menuService.setMenuValue();
-    }
+  public get menuObservable(): Observable<MenuInterface> {
+    return this.menuService.hideMenuObserver();
+  }
 
-    public ngOnInit(): void {
-        this.resizebledService.screenSize.subscribe((data) => {
-            this.isMobile = this.resizebledService.isMobile(data);
-        });
-    }
+  public hideSidebar(): void {
+    this.menuService.setMenuValue();
+  }
+
+  public ngOnInit(): void {
+    this.resizebledService.screenSize.subscribe((data) => {
+      this.isMobile = this.resizebledService.isMobile(data);
+    });
+  }
 }

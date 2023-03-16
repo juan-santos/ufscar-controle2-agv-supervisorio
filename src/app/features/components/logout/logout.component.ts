255921176894dd97de7aeb4b55e088767b1c20ab
Juan Santos <juan.h.santos@gmail.com>
2023-03-16 12:05:17 -0300

    feat: ajuste de logout

--- src/app/features/components/logout/logout.component.ts
+++ src/app/features/components/logout/logout.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProjectRoutes } from '../../enum/routes.enum';
 
 @Component({
     selector: 'app-logout',
     templateUrl: './logout.component.html',
     styleUrls: ['./logout.component.scss'],
 })
-export class LogoutComponent {}
+export class LogoutComponent implements AfterViewInit {
+
+  constructor(private router: Router){}
+
+  /**
+   * ngAfterViewInit
+  */
+  public ngAfterViewInit(): void {
+    this.router.navigate([ProjectRoutes.LOGIN]);
+  }
+}
